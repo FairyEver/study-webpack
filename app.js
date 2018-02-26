f339bba441ae02a47c8b18bf3aafc0b7bf6dac3b
李杨 <1711467488@qq.com>
2018-02-26 13:42:26 +0800

    no message

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ import sum from './src/sum'
 // CommonJS
 var minus = require('./src/minus')
 
+// AMD
+require(['./src/muti'], function (muti) {
+    console.log('muti(1, 2) = ', muti(1, 2))
+})
+
 console.log('sum(1, 2) = ', sum(1, 2))
 console.log('minus(1, 2) = ', minus(1, 2))
--- src/muti.js
+++ src/muti.js
@@ -1,7 +1,6 @@
-define([
-    'require',
-    'dependency'
-], function(require, factory) {
+define(function(require, factory) {
     'use strict';
-    
-});
+    return function (a, b) {
+        return a * b
+    }
+})
