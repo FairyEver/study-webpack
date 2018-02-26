538220c7da4cd944a897170b476d9acbd6eeb025
李杨 <1711467488@qq.com>
2018-02-26 13:48:28 +0800

    no message

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-// ES6
-import sum from './src/sum'
-
-// CommonJS
-var minus = require('./src/minus')
-
-// AMD
-require(['./src/muti'], function (muti) {
-    console.log('muti(1, 2) = ', muti(1, 2))
-})
-
-console.log('sum(1, 2) = ', sum(1, 2))
-console.log('minus(1, 2) = ', minus(1, 2))
+console.log('Hello')
